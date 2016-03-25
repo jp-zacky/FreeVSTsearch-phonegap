29b2a9e6a51b1f05fad2820a6cbcbb5114a7b3d2
zacky <yu-ya11nc@outlook.com>
2016-03-25 16:33:17 +0900

    duplicate clear

--- www/js/search.js
+++ www/js/search.js
@@ -2,12 +2,23 @@ var app_url = 'http://icon.jp/'
 
 $.get(app_url, function(data) {
   var content = $(data.responseText).find('h5');
+  var news = new Array();
+
 
   for (var i = 0; i < content.length; i++) {
     var domObject = $(content[i]).html();
-    var re = /.*無償.*/
-    if(domObject.search(re) != -1){
-      alert(domObject);
+
+    var re1 = /.*無償.*/
+    var re2 = /.*フリー.*/
+    if (domObject.search(re1) != -1 || domObject.search(re2) != -1) {
+      news.push(domObject);
     }
   }
+  news = news.filter(function(x, i, self) {
+    return self.indexOf(x) === i;
+  });
 });
+
+function format(domObject) {
+
+}
